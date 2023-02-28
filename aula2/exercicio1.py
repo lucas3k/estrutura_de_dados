7e1a52dfca6fd2c229c3c777904b969e54f469e1
lucas3k <lcamargostivan@gmail.com>
2023-02-27 22:19:35 -0300

    add exercicio

---
+++ aula2/exercicio1.py
@@ -0,0 +1,17 @@
+nome = "Lucas"
+idade = 21
+altura = 1.80
+peso = 68
+maior_de_idade = True
+
+print(
+  "Meu nome é",
+  nome,
+  "e tenho",
+  idade,
+  "anos. Minha altura é",
+  altura,
+  "e meu peso é",
+  peso,
+  "kg.",
+)
--- aula2/lucas.py
+++ aula2/lucas.py
@@ -31,3 +31,42 @@ print(type(3.14))
 print(type(True))
 
 # print(type(11, 1.1, True))
+
+# conversão de tipos de dados
+print(int(3.14))
+print(float(22))
+print(str(22))
+print(bool(0))
+print(bool(1))
+print(bool(22))
+print(bool(-22))
+print(bool(3.14))
+print(bool(0.0))
+print(bool(""))
+print(bool("oi meu chapa"))
+
+# operações matemáticas com conversões
+print(1 + 1)
+print(1 + 1.0)
+print(1.0 + 1.0)
+print(1 + int(1.0))
+print(1 + float(1))
+# print(1 + str(1))
+print(1 + bool(1))
+print(1 + bool(0))
+print(1 + bool(22))
+print(1 + bool(-22))
+print(1 + bool(3.14))
+print(1 + bool(0.0))
+print(1 + bool(""))
+
+# variáveis
+nome = "Lucas"
+idade = 21
+altura = 1.80
+gosta_de_cafe = True
+print("\n\n\n\n\n\n\n\n\n")
+print(nome)
+print(idade)
+print(altura)
+print(gosta_de_cafe)
