010bf0c3f1a7804fec018119c8c14af4d4f735a8
lucas3k <lcamargostivan@gmail.com>
2023-02-27 22:02:39 -0300

    types dates i python

---
+++ aula2/lucas.py
@@ -0,0 +1,33 @@
+# isso é um comentário heeee
+print("1 + 1")
+print(22, 33, 44)
+
+# colocar caracteres no lugar dos espaços
+print("1", "2", "3", sep="*")
+
+# tipos de dados string
+print("oi meu chapa")
+print("quero café")
+
+# tipo de dados int
+print(22)
+print(-33)
+print(0)
+
+# tipo de dados float
+print(3.14)
+print(1.0)
+print(0.0)
+
+# tipo de dados bool
+print(True)
+print(False)
+print(30 == 30)
+
+# verificar tipo de dados
+print(type("oi meu chapa"))
+print(type(22))
+print(type(3.14))
+print(type(True))
+
+# print(type(11, 1.1, True))
