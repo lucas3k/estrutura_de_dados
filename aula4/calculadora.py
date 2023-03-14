fb4a784eef21197c2a7e79378d419f27af0ea6fc
lucas3k <lcamargostivan@gmail.com>
2023-03-13 22:48:24 -0300

    finaly calculadora

--- aula4/calculadora.py
+++ aula4/calculadora.py
@@ -1,9 +1,23 @@
 sair = "Sair"
 
 while True:
-  num1 = float(input("Primeiro número: "))
-  num2 = float(input("Segundo número: "))
-  operador = input("Informe o operador (*-+/): ")
+  numValid = True
+
+  try:
+    num1 = float(input("Primeiro número: "))
+    num2 = float(input("Segundo número: "))
+    operador = input("Informe o operador (*-+/): ")
+  except:
+    numValid = False
+
+  if not numValid:
+    print("Número inválido. ")
+    continue
+
+  if operador not in ("*-+/"):
+    print("Operador não aceito")
+
+  print(eval(f"{num1} {operador} {num2}"))
 
   sair = input("Deseja sair? ").lower().startswith("s")
   if sair is True:
